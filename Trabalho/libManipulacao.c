37d8089bce729e729711df37357badbc3a1be35f
mc-gomes <bigmath2001@hotmail.com>
2021-09-09 00:18:01 -0300

    implementação da biblioteca

--- Trabalho/libManipulacao.c
+++ Trabalho/libManipulacao.c
@@ -2,21 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <locale.h>
+#include <ctype.h>
 #include "libManipulacao.h"
 
-// No momento todas as funções estão aqui
-// por causa de um problema com as bibliotecas
 
-
-void menu(){
-    printf("----------------------\n");
-    printf("\tMenu\n");
-    printf("1- Ler arquivo base\n");
-    printf("2- Gerar vocabulário\n");
-    printf("3- Sair\n");
-}
-/*
 int finalDePalavra(char pcar){
     int indx, pehfim = 0;
     char FIMPAL[8] = {' ', '.', ',', '-', ':', '/', '!', '?'};
@@ -331,65 +320,4 @@ void criaCorpoText(char nome_arq[]){
 
     fclose(fcorpo);
     fclose(plvs_notas);
-}
-*/
-
-
-int main(int argc, const char * argv[]){
-    setlocale(LC_ALL, "Portuguese");
-
-    FILE *arquivoRef;
-    char  nomeArquivo[30];
-
-    int opcao = 0;
-
-    do{
-        menu();
-        printf(">> ");
-        scanf("%d", &opcao);
-
-        switch (opcao)
-        {
-        case 1:
-            // LÊ O ARQUIVO INFORMADO PELO USUARIO
-            printf("Arquivo disponível: arquivo_reviews.csv\n");
-            printf("Informe o nome do arquivo (com a extensão): ");
-            scanf("%s", nomeArquivo);
-            arquivoRef = fopen(nomeArquivo, "r");
-            if(arquivoRef == NULL){
-                printf("ERRO: Não foi possível abrir o arquivo!\n");
-                return 0;
-            }
-            else{
-                separaPorNota(arquivoRef);
-                printf("\nArquivos gerados com sucesso! Pronto para gerar vocabulário.\n\n");
-                fclose(arquivoRef);
-            }
-            break;
-        case 2:
-            separaPalavra("nota1.txt");
-            separaPalavra("nota2.txt");
-            separaPalavra("nota3.txt");
-            separaPalavra("nota4.txt");
-            separaPalavra("nota5.txt");
-
-            criaCorpoText("N1semRep.txt");
-            criaCorpoText("N2semRep.txt");
-            criaCorpoText("N3semRep.txt");
-            criaCorpoText("N4semRep.txt");
-            criaCorpoText("N5semRep.txt");
-
-            printf("\nVocabulário criado com sucesso!\n");
-
-            break;
-        case 3:
-            printf("Programa encerrado!\n");
-            break;
-        default:
-            printf("Opção inválida!\n\n");
-            break;
-        }
-    } while (opcao != 3);
-    
-    return 0;
 }
--- Trabalho/libManipulacao.h
+++ Trabalho/libManipulacao.h
@@ -1,4 +1,39 @@
 #ifndef LIBMANIPULACAO_H
 #define LIBMANIPULACAO_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+/*  Essa função verfica se a variável recebida como parâmetro
+    é igual a algum caractere que marca final de palavra,
+    dentre os caracteres especificados.*/
+int finalDePalavra(char pcar);
+
+/*  Essa função analisa se 'plv' se repete dentro
+    do arquivo em que ela está (no caso 'filep').*/
+int analisaRepeticao(FILE *filep, char plv[], long int posic);
+
+/*  No contexto do trabalho, essa função lê o
+    arquivo .csv com a base de dados e cria os arquivos
+    separados pelas notas.*/
+void separaPorNota(FILE *arquivoReferencia);
+
+/*  Essa função recebe o nome de um arquivo de notas específico,
+    para que o mesmo possa ser lido e que cada uma de suas palavras
+    seja colocada em uma linha de outro arquivo '.txt'.*/
+void separaPalavra(char nomeArquivo[]);
+
+/*  Essa função recebe o arquivo que foi manipulado
+    pela função 'separaPalavra' e coloca as palavras
+    desse arquivo em outro, sem nenhuma repetição.*/
+void palavraUnica(FILE *frepetido, char nome_arq[]);
+
+/*  Função que gera o vocabulário com as palavras de
+    todos os arquivos de notas, sem nenhuma repetição.*/
+void criaCorpoText(char nome_arq[]);
+
+
 #endif
